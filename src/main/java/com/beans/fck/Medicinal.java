c2fe834822e1d6f90466699350755fc5d926cdef
leo <1579858856@qq.com>
2019-03-14 10:09:58 +0800

    314

--- src/main/java/com/beans/fck/Medicinal.java
+++ src/main/java/com/beans/fck/Medicinal.java
@@ -2,16 +2,133 @@ package com.beans.fck;
 
 public class Medicinal {
     private int mid;   // 药品标号
+    private String medid; //药品标号
     private String mname;  //药品名称
     private int tid;   //药品种类标号
+    private String tname;
     private Mtype mtype;  // 种类名称
-    private int mcount;   // 药品数量
+    private int mcount;   // 药品库存
+    private String mweight;  //重量单位
     private String mspec;  //药品规格
-    private String mfactory; //出产厂商
-    private String mdate;   //出产日期
+    private int sid; //出产厂商
+    private Supplier supplier;
+    private String sname;
+    private String mdate;   //创建日期
+    private double minprice; // 批发价
+    private double moutprice; // 处方价
+    private double minamount; // 批发额
+    private double moutamount; // 处方额
+    private String mjudge; //判断是否在维护
+
+    private String rnum;//采购编号
+    private int pid;// 采购序号
 
 
     public Medicinal() {
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public String getRnum() {
+        return rnum;
+    }
+
+    public void setRnum(String rnum) {
+        this.rnum = rnum;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getMedid() {
+        return medid;
+    }
+
+    public void setMedid(String medid) {
+        this.medid = medid;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public double getMinprice() {
+        return minprice;
+    }
+
+    public void setMinprice(double minprice) {
+        this.minprice = minprice;
+    }
+
+    public double getMoutprice() {
+        return moutprice;
+    }
+
+    public void setMoutprice(double moutprice) {
+        this.moutprice = moutprice;
+    }
+
+    public double getMinamount() {
+        return minamount;
+    }
+
+    public void setMinamount(double minamount) {
+        this.minamount = minamount;
+    }
+
+    public double getMoutamount() {
+        return moutamount;
+    }
+
+    public void setMoutamount(double moutamount) {
+        this.moutamount = moutamount;
+    }
+
+    public String getMweight() {
+        return mweight;
+    }
+
+    public void setMweight(String mweight) {
+        this.mweight = mweight;
+    }
+
+    public String getMjudge() {
+        return mjudge;
+    }
+
+    public void setMjudge(String mjudge) {
+        this.mjudge = mjudge;
     }
 
     public int getMid() {
@@ -62,13 +179,6 @@ public class Medicinal {
         this.mspec = mspec;
     }
 
-    public String getMfactory() {
-        return mfactory;
-    }
-
-    public void setMfactory(String mfactory) {
-        this.mfactory = mfactory;
-    }
 
     public String getMdate() {
         return mdate;
@@ -82,13 +192,25 @@ public class Medicinal {
     public String toString() {
         return "Medicinal{" +
                 "mid=" + mid +
+                ", medid='" + medid + '\'' +
                 ", mname='" + mname + '\'' +
                 ", tid=" + tid +
+                ", tname='" + tname + '\'' +
                 ", mtype=" + mtype +
                 ", mcount=" + mcount +
+                ", mweight='" + mweight + '\'' +
                 ", mspec='" + mspec + '\'' +
-                ", mfactory='" + mfactory + '\'' +
+                ", sid=" + sid +
+                ", supplier=" + supplier +
+                ", sname='" + sname + '\'' +
                 ", mdate='" + mdate + '\'' +
+                ", minprice=" + minprice +
+                ", moutprice=" + moutprice +
+                ", minamount=" + minamount +
+                ", moutamount=" + moutamount +
+                ", mjudge='" + mjudge + '\'' +
+                ", rnum='" + rnum + '\'' +
+                ", pid=" + pid +
                 '}';
     }
 }
