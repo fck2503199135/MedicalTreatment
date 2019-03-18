dd129182d3703e7719416dcd9e980b536b5f4142
leo <1579858856@qq.com>
2019-03-18 16:26:30 +0800

    318

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,60 @@
+package com.pojo;
+
+public class User {
+
+        private  int uid;
+        private  String username;
+        private String password;
+        private int pid;//职位表角色
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", pid=" + pid +
+                '}';
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
